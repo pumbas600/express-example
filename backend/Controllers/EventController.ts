235b82e0586f43a86563eac22e1a42147efd9402
pumbas600 <joshjeffers600@gmail.com>
2022-05-02 00:03:15 +1200

    Move post event validation to separate function

--- backend/Controllers/EventController.ts
+++ backend/Controllers/EventController.ts
@@ -1,6 +1,29 @@
 import { Request, Response } from 'express';
 import { PostEvent } from '../Types/Event';
 
+function isInvalidPostEvent(res: Response, postEvent: PostEvent): boolean {
+	const description = postEvent.description ?? '';
+	const errors: string[] = [];
+
+	// Validate the post request - Always assume that they've entered an invalid value
+	if (typeof postEvent.name !== 'string') {
+		errors.push(`The name must be a string, got ${typeof postEvent.name}`);
+	}
+	if (typeof postEvent.location !== 'string') {
+		errors.push(`The location must be a string, got ${typeof postEvent.location}`);
+	}
+	if (typeof description !== 'string') {
+		errors.push(`The description must be a string, got ${typeof description}`);
+	}
+
+	// Check for any errors
+	if (errors.length !== 0) {
+		res.status(401).json({ errors: errors });
+		return true;
+	}
+	return true;
+}
+
 /**
  * @desc 	Get all events
  * @route 	GET /api/events/
@@ -14,25 +37,7 @@ async function getEvents(req: Request, res: Response): Promise<void> {
  * @route 	POST /api/events/
  */
 async function createEvent(req: Request<{}, {}, PostEvent>, res: Response): Promise<void> {
-	const description = req.body.description ?? '';
-	const errors: string[] = [];
-
-	// Validate the post request - Always assume that they've entered an invalid value
-	if (typeof req.body.name !== 'string') {
-		errors.push(`The name must be a string, got ${typeof req.body.name}`);
-	}
-	if (typeof req.body.location !== 'string') {
-		errors.push(`The location must be a string, got ${typeof req.body.location}`);
-	}
-	if (typeof description !== 'string') {
-		errors.push(`The description must be a string, got ${typeof description}`);
-	}
-
-	// Check for any errors
-	if (errors.length !== 0) {
-		res.status(401).json({ errors: errors });
-		return;
-	}
+	if (isInvalidPostEvent(res, req.body)) return;
 
 	res.status(200).json({ message: 'Create event' });
 }
@@ -42,6 +47,8 @@ async function createEvent(req: Request<{}, {}, PostEvent>, res: Response): Prom
  * @route 	PUT /api/events/:id
  */
 async function updateEvent(req: Request<{ id: string }, {}, PostEvent>, res: Response): Promise<void> {
+	if (isInvalidPostEvent(res, req.body)) return;
+
 	res.status(200).json({ message: `Updating events for id ${req.params.id}` });
 }
 
