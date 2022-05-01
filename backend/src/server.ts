ed04bc82eaaa8fef739acb117f15b834251a88a2
pumbas600 <joshjeffers600@gmail.com>
2022-05-01 15:58:41 +1200

    Replace || with ??

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,11 +5,11 @@ import Config from './Types/Config';
 // Use require here so that we can type the config object
 const config: Config = require('../config.json');
 
-const port = config.port || 5000;
+const port = config.port ?? 5000;
 const app = express();
 
 app.use('/api/events', EventRouter);
-console.log('Hi');
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
 export default app;
